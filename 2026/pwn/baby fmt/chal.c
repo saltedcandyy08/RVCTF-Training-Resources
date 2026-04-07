4e76f28233022a919fb2f1b057910a12883ae465
Jakeboiegg <jake_ling_kae_eun@s2021.ssts.edu.sg>
2026-04-07 23:11:44 +0800

    t2w3 pwn
    temp training head bcuz pwn is my passion !

---
+++ 2026/pwn/baby fmt/chal.c
@@ -0,0 +1,26 @@
+//  gcc -o chal chal.c -fno-pie -no-pie -fstack-protector
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void win() {
+    execve("/bin/sh", NULL, NULL);
+}
+
+void vuln() {
+    char buffer[64];
+    printf("input: ");
+    fgets(buffer, sizeof(buffer)-1, stdin);
+
+    printf("you entered: ");
+    printf(buffer);
+    puts("");
+}
+
+int main() {
+    setbuf(stdin, NULL);
+    setbuf(stdout, NULL);
+    vuln();
+    return 0;
+}
